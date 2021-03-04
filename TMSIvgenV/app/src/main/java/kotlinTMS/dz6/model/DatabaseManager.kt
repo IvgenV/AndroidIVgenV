8fecc67b35907bf4d7e85d1a727c677f5cae23ee
Ivgen <ivgenij.v@gmail.com>
2021-03-04 21:31:28 +0300

    DZ6

---
+++ TMSIvgenV/app/src/main/java/kotlinTMS/dz6/model/DatabaseManager.kt
@@ -0,0 +1,29 @@
+package kotlinTMS.dz6.model
+
+import android.content.Context
+import android.content.Context.MODE_PRIVATE
+import android.net.Uri
+import java.net.URL
+
+class DatabaseManager(context: Context) {
+
+    var sp = context.getSharedPreferences("Shared", MODE_PRIVATE)
+
+
+    fun getage():Int{
+        return sp.getInt("age",370)
+    }
+
+    fun changeAge(newAge:Int){
+        sp.edit().putInt("age",newAge).commit()
+    }
+
+    fun getUrl():String{
+        return  sp.getString("avatar","https://media.istockphoto.com/vectors/error-page-or-file-not-found-icon-vector-id924949200")!!
+    }
+
+    fun changeUrl(url:String){
+        sp.edit().putString("avatar",url).commit()
+    }
+
+}
---
+++ TMSIvgenV/app/src/main/java/kotlinTMS/dz6/view/Dz6MainView.kt
@@ -0,0 +1,12 @@
+package kotlinTMS.dz6.view
+
+import java.net.URL
+
+interface Dz6MainView {
+
+    fun setAge(age:Int)
+    fun changeAge():Int
+    fun setAvatar(url:String)
+    fun changeUrl():String
+
+}
